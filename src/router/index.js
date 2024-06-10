8a35ed9e6b5b0a0e8684939f7d6a6be850da2d6b
thu <luuthikimthu2002@gmail.com>
2024-06-10 12:51:53 +0700

    update chi tiet san pham

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,75 @@
 import HomePages from "../pages/Homepages/HomePages";
+import { ProductDetailComponent } from "../components/ProductDetailComponent/ProductDetailComponent";
+import { aProductDetailComponent } from "../components/ProductDetailComponent/1ProductDetailComponent";
+import { bProductDetailComponent } from "../components/ProductDetailComponent/2ProductDetailComponent";
+import { cProductDetailComponent } from "../components/ProductDetailComponent/3ProductDetailComponent ";
+import { dProductDetailComponent } from "../components/ProductDetailComponent/4ProductDetailComponent ";
+import { eProductDetailComponent } from "../components/ProductDetailComponent/5ProductDetailComponent ";
 
 export const routes = [
     {
         path: '/',
         page: HomePages,
+        isShowHeader: true
+    },
+    {
+        path: '/product-details1',
+        page: ProductDetailComponent,
+        isShowHeader: true
+    },
+    {
+        path: '/product-details2',
+        page: aProductDetailComponent,
+        isShowHeader: true
+    },
+    {
+        path: '/product-details3',
+        page: bProductDetailComponent,
+        isShowHeader: true
+    },
+    {
+        path: '/product-details4',
+        page: cProductDetailComponent,
+        isShowHeader: true
+    },
+    {
+        path: '/product-details5',
+        page: dProductDetailComponent,
+        isShowHeader: true
+    },
+    {
+        path: '/product-details6',
+        page: eProductDetailComponent,
+        isShowHeader: true
+    },
+    {
+        path: '/:type/product-details1',
+        page: ProductDetailComponent,
+        isShowHeader: true
+    },
+    {
+        path: '/:type/product-details2',
+        page: aProductDetailComponent,
+        isShowHeader: true
+    },
+    {
+        path: '/:type/product-details3',
+        page: bProductDetailComponent,
+        isShowHeader: true
+    },
+    {
+        path: '/:type/product-details4',
+        page: cProductDetailComponent,
+        isShowHeader: true
+    },
+    {
+        path: '/:type/product-details5',
+        page: dProductDetailComponent,
+        isShowHeader: true
+    },
+    {
+        path: '/:type/product-details6',
+        page: eProductDetailComponent,
         isShowHeader: true
     }
 ]
